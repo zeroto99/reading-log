75238187a3fa79556d79164a302c0c6151581f7c
zeroto99 <myv_01@naver.com>
2022-05-20 22:35:54 +0900

    💄 reset.css을 createGlobalStyle로 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import './css/global.css';
+import './style/font.css';
 import Home from './pages/Home';
+import GlobalStyle from './style/global';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Home />
     </div>
   );
